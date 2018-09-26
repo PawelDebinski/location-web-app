2037642de94bdcc585715c07ad33355cd4b78df1
Pawel Debinski <spokojasne@interia.eu>
2018-09-26 14:41:00 +0200

    added success response message, closes #10

--- src/main/java/pl/pawel/location/controllers/LocationController.java
+++ src/main/java/pl/pawel/location/controllers/LocationController.java
@@ -2,6 +2,7 @@ package pl.pawel.location.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +21,10 @@ public class LocationController {
     }
 
     @PostMapping("/saveLoc")
-    public String saveLocation(@ModelAttribute("location")Location location) {
-        service.saveLocation(location);
+    public String saveLocation(@ModelAttribute("location")Location location, ModelMap modelMap) {
+        Location locationSaved = service.saveLocation(location);
+        String msg = "Location saved with id: " + locationSaved.getId();
+        modelMap.addAttribute("msg", msg);
         return "createLocation";
     }
 }
