db1834351a1ea5b393a8e58dcd6bebf22a11bc34
Pawel Debinski <spokojasne@interia.eu>
2018-09-27 10:20:21 +0200

    Utils -JFreeCharts - updated repo, closes #22

--- src/main/java/pl/pawel/location/repos/LocationRepository.java
+++ src/main/java/pl/pawel/location/repos/LocationRepository.java
@@ -2,8 +2,14 @@ package pl.pawel.location.repos;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.pawel.location.entities.Location;
 
+import java.util.List;
+
 public interface LocationRepository extends JpaRepository<Location, Integer> {
+
+    @Query("select type, count(type) from location group by type")
+    public List<Object[]> findTypeAndTypeCount();
 
 }
